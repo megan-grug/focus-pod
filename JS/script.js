c8e44ae19630f81125e9a7bd89ccb5d24494844a
megan-grug <meganjones.cymru@gmail.com>
2021-08-24 21:36:44 +0000

    basic dark mode in place, function icons added

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,25 @@ function addNew() {
   let newListItem = document.createElement("li");
   
   todoList.appendChild(newListItem);
+
   let inputValue = document.getElementById('inputBox').value;
  // let newTask = document.getElementById('inputBox').value;
 
-  newListItem.innerHTML = inputBox.value;
-  `<input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..."></input> 
+  newListItem.innerHTML = `<p>`+ inputBox.value + `</p>` +
+  `<input class="form-check-input" type="checkbox" value="" aria-label="..."></input> 
   <button class="editBtn"><i class="fas fa-edit"></i></button>
   <button class="deleteBtn"><i class="fas fa-trash-alt"></i></button>`;
-  
   inputBox.value = " ";
-}
- 
+};
+
+const tickBox = document.getElementsByClassName("form-check-input");
+
+tickBox.onclick = ()=>{
+  $(this).prev().addClass("completed");
+};
+
+//darkMode function from W3 Schools (https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp)
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+};
